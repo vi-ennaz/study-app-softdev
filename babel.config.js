9da947f7160ec3860085f209dbe64e8bfaea0496
vi-ennaz <viennalokie@gmail.com>
2026-08-09 20:33:15 +1000

    babel.config.js internal doc

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+// This is the Babel configuration file for the Expo project,
+// which specifies the presets and plugins to be used by Babel for transpiling the JavaScript code
 module.exports = function(api) {
   api.cache(true);
   return {
